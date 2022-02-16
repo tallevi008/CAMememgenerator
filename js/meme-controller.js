e45ab38e3e0289017694a84c2592e53ca97ea148
TalL <tallevi008@gmail.com>
2022-02-16 14:56:35 +0200

    * changing meme img folder name
    *setting the model

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,24 @@
 'use-strict';
+
+var gElCanvas;
+var gCtx;
+
+function Oninit() {
+    gElCanvas = document.querySelector('canvas');
+    gCtx = gElCanvas.getContext('2d');
+    renderMeme();
+}
+
+function renderMeme() {
+    drawImg();
+
+}
+
+function drawImg() {
+    var img = new Image();
+    img.src = getImgForDisplay();
+
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    };
+}
