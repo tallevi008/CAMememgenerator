2591b343739e0379c6b1512d2cacb8befb0759f3
TalL <tallevi008@gmail.com>
2022-02-20 12:19:26 +0200

    update img model

--- js/services/memeService.js
+++ js/services/memeService.js
@@ -5,22 +5,22 @@ var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 var gImgs = [
     { id: 1, url: 'img/meme-imgs/1.jpg', keywords: ['funny', 'plitical'] },
     { id: 2, url: 'img/meme-imgs/2.jpg', keywords: ['cute', 'animals'] },
-    { id: 3, url: 'img/meme-imgs/3.jpg', keywords: ['cute', 'animals'] },
+    { id: 3, url: 'img/meme-imgs/3.jpg', keywords: ['cute', 'animals', 'baby'] },
     { id: 4, url: 'img/meme-imgs/4.jpg', keywords: ['cute', 'animals'] },
-    { id: 5, url: 'img/meme-imgs/5.jpg', keywords: ['cute', 'animals'] },
-    { id: 6, url: 'img/meme-imgs/6.jpg', keywords: ['cute', 'animals'] },
-    { id: 7, url: 'img/meme-imgs/7.jpg', keywords: ['cute', 'animals'] },
-    { id: 8, url: 'img/meme-imgs/8.jpg', keywords: ['cute', 'animals'] },
-    { id: 9, url: 'img/meme-imgs/9.jpg', keywords: ['cute', 'animals'] },
-    { id: 10, url: 'img/meme-imgs/10.jpg', keywords: ['cute', 'animals'] },
-    { id: 11, url: 'img/meme-imgs/11.jpg', keywords: ['cute', 'animals'] },
-    { id: 12, url: 'img/meme-imgs/12.jpg', keywords: ['cute', 'animals'] },
-    { id: 13, url: 'img/meme-imgs/13.jpg', keywords: ['cute', 'animals'] },
-    { id: 14, url: 'img/meme-imgs/14.jpg', keywords: ['cute', 'animals'] },
-    { id: 15, url: 'img/meme-imgs/15.jpg', keywords: ['cute', 'animals'] },
-    { id: 16, url: 'img/meme-imgs/16.jpg', keywords: ['cute', 'animals'] },
-    { id: 17, url: 'img/meme-imgs/17.jpg', keywords: ['cute', 'animals'] },
-    { id: 18, url: 'img/meme-imgs/18.jpg', keywords: ['cute', 'animals'] },
+    { id: 5, url: 'img/meme-imgs/5.jpg', keywords: ['cute', 'baby'] },
+    { id: 6, url: 'img/meme-imgs/6.jpg', keywords: ['funny', 'sarcastic'] },
+    { id: 7, url: 'img/meme-imgs/7.jpg', keywords: ['cute', 'baby'] },
+    { id: 8, url: 'img/meme-imgs/8.jpg', keywords: ['funny', 'movie'] },
+    { id: 9, url: 'img/meme-imgs/9.jpg', keywords: ['funny', 'baby'] },
+    { id: 10, url: 'img/meme-imgs/10.jpg', keywords: ['funny', 'plitical'] },
+    { id: 11, url: 'img/meme-imgs/11.jpg', keywords: ['sarcastic', 'sport'] },
+    { id: 12, url: 'img/meme-imgs/12.jpg', keywords: ['sarcastic', 'funny'] },
+    { id: 13, url: 'img/meme-imgs/13.jpg', keywords: ['movie', 'funny'] },
+    { id: 14, url: 'img/meme-imgs/14.jpg', keywords: ['movie', 'sarcastic'] },
+    { id: 15, url: 'img/meme-imgs/15.jpg', keywords: ['movie', 'sarcastic'] },
+    { id: 16, url: 'img/meme-imgs/16.jpg', keywords: ['movie', 'funny'] },
+    { id: 17, url: 'img/meme-imgs/17.jpg', keywords: ['sarcastic', 'plitical'] },
+    { id: 18, url: 'img/meme-imgs/18.jpg', keywords: ['cute', 'movie'] },
 ];
 
 var gMeme = {
@@ -49,21 +49,16 @@ function getImgForDisplay(imgId) {
     return gImgs.find(img => imgId === img.id);
 }
 
-//Upadte gMeme text line
-function setLineTxt(line) {
-    var lineIdx = gMeme.selectedLineIdx;
-    gMeme.lines[lineIdx].txt = line;
+// Change meme attribute
+function setMemeAttr(attr, val) {
+    var memeAtrr = gMeme.lines[gMeme.selectedLineIdx];
+    memeAtrr[attr] = val;
 }
 
 //updating the id of the selcted img in the gMeme object
 function setImg(imgId) {
     gMeme.selectedImgId = +imgId;
 
-}
-// Change color of font or stroke
-function setColor(colorPurpose, selectedColor) {
-    var memeAtrr = gMeme.lines[gMeme.selectedLineIdx];
-    memeAtrr[colorPurpose] = selectedColor;
 }
 
 function setFontSize(diff) {
@@ -84,13 +79,13 @@ function addLine() {
         pos: { x: null, y: null }
     })
 
-    //to switch to the new line
+    //Switch to the new line
     gMeme.selectedLineIdx = (gMeme.lines).length - 1;
 };
 
 function removeLine() {
     (gMeme.lines).splice(gMeme.selectedLineIdx, 1);
-    //to switch to the previos line
+    //Switch to the prev line
     gMeme.selectedLineIdx = (gMeme.lines).length - 1;
 }
 
@@ -111,3 +106,10 @@ function onSetLinePos(x, y, lineIdx) {
     gMeme.lines[lineIdx].pos.x = x;
     gMeme.lines[lineIdx].pos.y = y;
 }
+
+
+//Upadte gMeme text line
+// function setLineTxt(line) {
+//     var lineIdx = gMeme.selectedLineIdx;
+//     gMeme.lines[lineIdx].txt = line;
+// }
